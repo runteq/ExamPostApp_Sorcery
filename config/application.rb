8ca36e86e78d6bc7a00395d4b603c8be8873e616
yuji91 <yuji91.1227@gmail.com>
2021-04-16 15:24:26 +0900

    Modify application config

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,18 @@ module PostAppSorcery
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework :rspec,
+      fixtures: true,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      controller_specs: false,
+      request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
