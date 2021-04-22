deb4907afc2e661a3599bc807f719669af38f46e
yuji91 <yuji91.1227@gmail.com>
2021-04-22 23:39:44 +0900

    Fix flash message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
     @post = current_user.posts.build(post_params)
 
     if @post.save
-      redirect_to @post, notice: 'Post was successfully created.'
+      redirect_to @post, success: 'Post was successfully created.'
     else
       render :new
     end
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   def update
     if @post.update(post_params)
-      redirect_to @post, notice: 'Post was successfully updated.'
+      redirect_to @post, success: 'Post was successfully updated.'
     else
       render :edit
     end
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   def destroy
     @post.destroy
-    redirect_to posts_url, notice: 'Post was successfully destroyed.'
+    redirect_to posts_url, success: 'Post was successfully destroyed.'
   end
 
   private
